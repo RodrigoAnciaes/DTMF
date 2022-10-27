6b10fa3a85b1b7fa356f8bde6cd610228220e848
RodrigoAnciaes <jogarlolsecundario@gmail.com>
2022-10-27 15:00:27 -0300

    Update encode_versaoAlunos.py

--- encode_versaoAlunos.py
+++ encode_versaoAlunos.py
@@ -20,7 +20,8 @@ def todB(s):
 
 def main():
     
-   
+    signal = signalMeu()
+
     #********************************************instruções*********************************************** 
     # seu objetivo aqui é gerar duas senoides. Cada uma com frequencia corresposndente à tecla pressionada
     # então inicialmente peça ao usuário para digitar uma tecla do teclado numérico DTMF
@@ -36,16 +37,41 @@ def main():
     
 
     print("Inicializando encoder")
+    dicFrequencies = {'1':(697,1209), '2':(697,1336), '3':(697,1477), 'A':(697,1633), '4':(770,1209), '5':(770,1336), '6':(770,1477), 'B':(770,1633), '7':(852,1209), '8':(852,1336), '9':(852,1477), 'C':(852,1633),'0':(941,1336), '#':(941,1477), 'D':(941,1633)}
     print("Aguardando usuário")
+    key = input("Digite uma tecla do teclado numérico DTMF: ")
+    print("Tecla digitada: ", key)
     print("Gerando Tons base")
+    fs = 48000
+    duration = 1
+    t = np.linspace(0, duration, duration*fs, False)
+    print("Gerando Tons")
+    tone1 = np.sin(dicFrequencies[key][0] * 2 * np.pi * t)
+    tone2 = np.sin(dicFrequencies[key][1] * 2 * np.pi * t)
+    print("Somando Tons")
+    tone = tone1 + tone2
     print("Executando as senoides (emitindo o som)")
-    print("Gerando Tom referente ao símbolo : {}".format(NUM))
-    sd.play(tone, fs)
+    print("Gerando Tom referente ao símbolo : {}".format(key))
+    #sd.play(tone, fs)
+    sd.play(tone, fs, loop = True)
+    sd.wait()
     # Exibe gráficos
+    print("Gerando gráficos")
+    plt.figure(1)
+    plt.title("Sinal")
+    plt.xlabel("Tempo")
+    plt.ylabel("Amplitude")
+    plt.plot(tone)
+    plt.figure(2)
+    plt.title("Transformada de Fourier")
+    plt.xlabel("Frequência")
+    plt.ylabel("Amplitude")
+    x,y = signal.calcFFT(tone, fs)
+    plt.plot(x,y)
     plt.show()
     # aguarda fim do audio
     sd.wait()
-    plotFFT(self, signal, fs)
+    #plotFFT(self, signal, fs)
     
 
 if __name__ == "__main__":
